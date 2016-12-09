535e83828116bd43213a6d4083f829f87bc21204
arnaud longueval <arnaud.longueval@edhec.com>
2016-12-09 16:06:26 +0100

    heroku done FB done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208104013) do
+ActiveRecord::Schema.define(version: 20161209145422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,8 +100,6 @@ ActiveRecord::Schema.define(version: 20161208104013) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.string   "first_name"
-    t.string   "last_name"
     t.datetime "date_birth"
     t.string   "place_birth"
     t.string   "nationality"
@@ -111,6 +109,13 @@ ActiveRecord::Schema.define(version: 20161208104013) do
     t.string   "zip_code"
     t.string   "phone"
     t.boolean  "admin",                  default: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -128,6 +133,8 @@ ActiveRecord::Schema.define(version: 20161208104013) do
     t.integer  "rente"
     t.integer  "horizon"
     t.float    "tri"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["old_id"], name: "index_viagers_on_old_id", using: :btree
   end
 
