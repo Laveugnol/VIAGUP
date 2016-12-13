f388a6ae12acc74e9f693ea819c3601c81a6d39c
arnaud longueval <arnaud.longueval@edhec.com>
2016-12-13 18:51:21 +0100

    martin nous a sauvé , hallejuha

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,8 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require jquery.ui.widget
 //= require jquery.iframe-transport
-//= require chart.easypiechart/dist/jquery.easypiechart.min
-//= require scripts.js
-//= require jquery.themepunch.tools.min.js
-//= require demo.revolution_slider.js
-//= require owl-carousel/owl.carousel
+//= require scripts
 
 
 
