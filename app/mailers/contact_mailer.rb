c0e58cc3c65bce254ed1e88e9a801fc8037759cf
arnaud longueval <arnaud.longueval@edhec.com>
2017-05-05 15:47:25 +0200

    humhum

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,7 @@ class ContactMailer < ApplicationMailer
   def contact_form(contact)
     @contact = contact  # Instance variable => available in view
 
-    mail(to: "arnaud@viagup.com", subject: "Contact Viag'up: #{@contact.subject}", from: "#{@contact.email})
+    mail(to: "arnaud@viagup.com", subject: "Contact Viag'up: #{@contact.subject}", from: "#{@contact.email}")
 
   end
 
